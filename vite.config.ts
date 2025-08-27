77bd77b053cdbb8c914c854c4ed97f30650a78a5
louis <dian819864938@126.com>
2025-08-27 11:19:30 +0800

    Update Vite configuration to include additional entry points for front-daily and back-recharge HTML files.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 
-// 多页面输入：入口(index.html)、正面(front.html)、反面(back.html)
+// 多页面输入：入口(index.html)、正面(front.html)、反面(back.html)、正面Daily(front-daily.html)、反面可增值(back-recharge.html)
 export default defineConfig({
   build: {
     rollupOptions: {
@@ -9,6 +9,8 @@ export default defineConfig({
         index: resolve(__dirname, 'index.html'),
         front: resolve(__dirname, 'front.html'),
         back: resolve(__dirname, 'back.html'),
+        frontDaily: resolve(__dirname, 'front-daily.html'),
+        backRecharge: resolve(__dirname, 'back-recharge.html'),
       },
     },
   },
